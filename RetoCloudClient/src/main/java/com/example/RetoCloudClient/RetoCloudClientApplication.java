71eb5021f1228d3ff299aec6ecfe44ed8bc951a0
jesusmiguelgarciaduran <jesusmiguelgarciaduran@gmail.com>
2021-10-22 12:38:31 +0200

    Cliente creado

---
+++ RetoCloudClient/src/main/java/com/example/RetoCloudClient/RetoCloudClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.RetoCloudClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoCloudClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoCloudClientApplication.class, args);
+	}
+
+}
---
+++ RetoCloudClient/src/test/java/com/example/RetoCloudClient/RetoCloudClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RetoCloudClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoCloudClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
